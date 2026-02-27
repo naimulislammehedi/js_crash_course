cadf73542595b582d279481c44cf2fd3334a8384
Mehedi <mehedi.interface@gmail.com>
2026-02-28 04:10:46 +0600

    12 - Arrays

---
+++ 11_arrays.js
@@ -0,0 +1,10 @@
+// 12 - Arrays
+// Store multiple values in one variable. 
+let numbers = [10, 20, 30]; 
+
+console.log(numbers[0]); 
+numbers.push(40); // add 
+console.log(numbers); 
+numbers.pop(); 
+console.log(numbers); 
+console.log(numbers.length); 

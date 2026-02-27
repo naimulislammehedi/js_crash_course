2bc6a2c85db0ff72b48fda66e0ca590d292f9d7f
Mehedi <mehedi.interface@gmail.com>
2026-02-28 04:30:41 +0600

    13 - JS Objects

---
+++ 13_objects.js
@@ -0,0 +1,10 @@
+// 13 - Objects
+// Store data in key-value pairs
+let user = {
+    name: 'Mehedi', 
+    age: 20, 
+    country: 'Bangladesh'
+}
+
+console.log(user.name); // dot notation 
+console.log(user['age']); // bracket notation 

5534ae08c1658a289911a52d176094ed232c72a6
Mehedi <mehedi.interface@gmail.com>
2026-02-28 02:59:44 +0600

    3 - Data Types (Primitive & Non-Primitive)

---
+++ 3_data_types.js
@@ -0,0 +1,11 @@
+// 3 - Data Types (Primitive & Non-Primitive)
+// Primive 
+let name = "John"; // string 
+let age = 25; // number 
+let isAdmin = true; // boolean 
+let x = null; // null 
+let y; // undefined 
+
+// Non-Primitive
+let arr = [1, 2, 3]; // array 
+let user = {name: "John", age: 25}; // object 

62f98305ffa5764a1ce95efcd5551e624ae788d8
Mehedi <mehedi.interface@gmail.com>
2026-02-28 04:07:18 +0600

    10 - Template Strings

---
+++ 10_template_strings.js
@@ -0,0 +1,14 @@
+// 10 - Template Strings
+// Used to dynamic and multiline strings 
+let name = "Mehedi"; 
+let age = 25; 
+
+let message = `My name is ${name} and I am ${age} years old.`; 
+console.log(message); 
+
+// Multiline 
+let text = 
+`Hello
+This is line 1
+This is line 2`; 
+console.log(text); 

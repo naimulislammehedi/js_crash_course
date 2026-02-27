f76acbded899c049558d3f347021613b92bb626f
Mehedi <mehedi.interface@gmail.com>
2026-02-28 02:56:46 +0600

    2 - Variables (var, let, const)

---
+++ 2_variables.js
@@ -0,0 +1,14 @@
+// 2 - Variables (var, let, const)
+// Used to store data. 
+var name = "Mehedi"; // old way (avoid in modern JS)
+let age = 26; // can change value  
+const country = "Bangladesh"; // cannot reassign 
+
+
+let score = 10; 
+score = 20; // allowed 
+
+const pi = 3.14; 
+pi = 3.1416; // not allowed 
+
+

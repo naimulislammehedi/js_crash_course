64eb64bd2cd62b243de9aa0284e22420a59be7e4
Mehedi <mehedi.interface@gmail.com>
2026-02-28 13:34:03 +0600

    25 - reduce

---
+++ 25_reduce.js
@@ -0,0 +1,6 @@
+// 25 - reduce 
+// Reduces an array to a single value (sum, product, etc.)
+const numbers = [1, 2, 3, 4]; 
+
+const sum = numbers.reduce((acc, curr) => acc + curr, 0); 
+console.log(sum); 

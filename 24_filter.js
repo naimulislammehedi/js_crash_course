cb7b0bd858d6e446de8edfe4beea8935e804f227
Mehedi <mehedi.interface@gmail.com>
2026-02-28 13:26:58 +0600

    24 - filter

---
+++ 24_filter.js
@@ -0,0 +1,6 @@
+// 24 - filter
+// Selects elements that satisfy a condition. Returns a new array. 
+const numbers = [1, 2, 3, 4, 5, 6]; 
+
+const evenNumbers = numbers.filter(num => num % 2 === 0); 
+console.log(evenNumbers); 

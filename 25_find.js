61ee0cde9cf84d669aaf77d0b77afa04413b1f70
Mehedi <mehedi.interface@gmail.com>
2026-02-28 13:29:55 +0600

    25 - find

---
+++ 25_find.js
@@ -0,0 +1,7 @@
+// 25 - find 
+// Returns the first element that satisfies a condition. 
+
+const numbers = [5, 12, 8, 130, 44]; 
+
+const firstLarge = numbers.find(num => num > 10); 
+console.log(firstLarge); 
